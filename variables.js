222d245743bc857ef3a2e705bb4bae798e750fb2
ajaykumar3368 <ajay.kumar@mindgate.in>
2025-07-22 16:37:35 +0530

    scope of variables

--- variables.js
+++ variables.js
@@ -2,3 +2,11 @@
 // 1. let 
 // 2. const
 // 3.var
+
+
+// Scope of variables
+
+// The scope of a variable defines the region of the program where it can be accessed or modified. There are generally two main types: 
+
+// 1. Local Scope:_ Variables declared within a specific function or block of code and are only accessible within that context.
+// 2:- Global Scope: Variables declared outside any function or block and are accessible throughout the entire program.
